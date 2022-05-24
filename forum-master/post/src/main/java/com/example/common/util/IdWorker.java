1928dc680cd285b87c73bd1db8ba98d4e877d67e
infinite-war <75058131+infinite-war@users.noreply.github.com>
2022-05-24 22:39:24 +0800

    Update IdWorker.java
    24行和27行id最大值简写

--- forum-master/post/src/main/java/com/example/common/util/IdWorker.java
+++ forum-master/post/src/main/java/com/example/common/util/IdWorker.java
@@ -21,10 +21,10 @@ public class IdWorker {
     private final static long datacenterIdBits = 2L;
 
     //机器ID最大值
-    private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    private final static long maxWorkerId = ~(-1L << workerIdBits);
 
     //数据中心ID最大值
-    private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    private final static long maxDatacenterId = ~(-1L << datacenterIdBits);
 
     //毫秒内自增位
     private final static long sequenceBits = 4L;
